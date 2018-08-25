8dba7f250a571718092f077d61724506a4b2a7b5
Sergey Baboshin <ijustbsd@gmail.com>
2018-08-25 19:15:40 +0300

    Language changed to ru

--- ipt_connect/ipt_connect/settings.py
+++ ipt_connect/ipt_connect/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 LOCALE_PATHS = (
     os.path.join('locale'),
