46e102826f4c6ecdc778778eeb538f3043eaf77e
Sergey Baboshin <ijustbsd@gmail.com>
2018-08-27 22:21:06 +0300

    Set number of physics fights in Repnoe 9

--- ipt_connect/Repnoe2018_9/parameters.py
+++ ipt_connect/Repnoe2018_9/parameters.py
@@ -8,8 +8,8 @@ NAME = {
 }
 
 # Models parameters
-npf = 4                 # Number of Physics fights
-with_final_pf = True    # Is there a Final Fight ?
+npf = 2                 # Number of Physics fights
+with_final_pf = False    # Is there a Final Fight ?
 reject_malus = 0.2      # Malus for too many rejections
 npfreject_max = 3       # Maximum number of tactical rejection (per fight)
 netreject_max = 1       # Maximum number of eternal rejection
