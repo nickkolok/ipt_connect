212724145a2bc073c5c49a0c7541b7148f58eeb5
Sergey Baboshin <ijustbsd@gmail.com>
2018-08-27 21:51:41 +0300

    Final was disabled

--- ipt_connect/Repnoe2018_7/parameters.py
+++ ipt_connect/Repnoe2018_7/parameters.py
@@ -9,7 +9,7 @@ NAME = {
 
 # Models parameters
 npf = 4                 # Number of Physics fights
-with_final_pf = True    # Is there a Final Fight ?
+with_final_pf = False    # Is there a Final Fight ?
 reject_malus = 0.2      # Malus for too many rejections
 npfreject_max = 3       # Maximum number of tactical rejection (per fight)
 netreject_max = 1       # Maximum number of eternal rejection
